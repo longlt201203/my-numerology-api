7df3b039a39d636d96d02a286eca434c7de0c4e4
Le Thanh Long <longlt201203@gmail.com>
2024-05-21 21:09:17 +0700

    feat: 🎸 update numerology service

--- src/numerology/numerology.service.ts
+++ src/numerology/numerology.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import unidecode from "unidecode";
 
 @Injectable()
 export class NumerologyService {
@@ -14,5 +15,26 @@ export class NumerologyService {
         const yearSum = this.recursionDigitSum(dob.getFullYear());
         const resultNumber = this.recursionDigitSum(daySum+monthSum+yearSum);
         return resultNumber;
+    }
+
+    calculateSoulUrgeNumber(fullName: string) {
+        const charMap = {
+            a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10,
+            k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20,
+            u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+        };
+
+        let sum = 0;
+        unidecode(fullName.toLowerCase())
+            .split('')
+            .forEach((item) => {
+                if (charMap[item]) sum += charMap[item];
+            });
+        
+        return this.recursionDigitSum(sum);
+    }
+
+    calculateExpressionNumber(fullName: string) {
+        
     }
 }
